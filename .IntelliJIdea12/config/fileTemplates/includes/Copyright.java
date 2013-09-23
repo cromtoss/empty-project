97777e969ac36f1998f8bcafd6747f704a56197d
Thomas A. Cross <tom@cromtoss.us>
2013-09-23 10:19:45 -0500

    Added typical files for an empty project.

---
+++ .IntelliJIdea12/config/fileTemplates/includes/Copyright.java
@@ -0,0 +1,8 @@
+#set($Revision = '$Revision')
+#set($DateTime = '$DateTime')
+#set($Header = '$Header')
+#set($Author = '$Author')
+/*	$Header: $
+ *
+ *	Copyright © ${YEAR} Pearson VUE. All rights reserved.
+ */
---
+++ .IntelliJIdea12/config/fileTemplates/includes/File Header.java
@@ -0,0 +1,6 @@
+/**
+ *  //tcTODO
+ *
+ *  @author   Tom Cross
+ *  @version $Revision: #1 $ submitted $DateTime: ${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}:00 $ by $Author: ${USER} $	
+ */
